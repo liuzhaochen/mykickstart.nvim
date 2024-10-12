b06abe2e75817896f0e2b0bdb2fce26628f4c10c
liuzhaochen <liuzcphy@gmail.com>
2024-10-12 19:35:07 +0800

    add several plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+
 --[[
 
 =====================================================================
@@ -650,6 +651,15 @@ require('lazy').setup({
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+      require'lspconfig'.julials.setup{
+        on_new_config = function(new_config, _)
+            local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
+            if require'lspconfig'.util.path.is_file(julia) then
+          vim.notify("Hello!")
+                new_config.cmd[1] = julia
+            end
+        end
+      }
       require('mason-lspconfig').setup {
         handlers = {
           function(server_name)
@@ -918,10 +928,10 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
@@ -929,7 +939,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
